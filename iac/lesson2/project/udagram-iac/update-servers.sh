10245f81ad17a06b08b24af83615cf7d5cd7c951
Gimmy <gamalgiali@gmail.com>
2021-08-25 20:49:54 -0400

    Structure update

--- iac/lesson2/project/udagram-iac/update-servers.sh
+++ iac/lesson2/project/udagram-iac/update-servers.sh
@@ -3,7 +3,7 @@ REGION=us-east-1
 
 aws cloudformation update-stack \
 --stack-name $STACK_NAME \
---template-body file://stack.yml \
---parameters file://parameters.json \
+--template-body file://servers.yml \
+--parameters file://parameters-servers.json \
 --region=$REGION \
 --capabilities CAPABILITY_IAM
